34016c2b19b8278ac2cbe015c33f7e49422a20ac
Michael McNamara <michael@oldmasterhands.com>
2022-02-20 22:05:22 -0800

    Created new user event dcript for employee

---
+++ src/FileCabinet/SuiteScripts/sdr_ue_employee.js
@@ -0,0 +1,25 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ */
+ define([],
+
+    function() {
+
+        return {
+        	afterSubmit : function(context) {
+ //       		log.debug('Hello World');
+   var employee = context.newRecord;
+   var empCode        = employee.getValue('custentity_sdr_employee_code');
+   var supervisorName = employee.getText('supervisor');
+   var supervisorId   = employee.getValue('supervisor');
+   employee.setValue('custentity_sdr_employee_code')
+
+   log.debug('Employee Code',empCode);
+   log.debug('Supervisor Name',supervisorName);
+   log.debug('Supervisor ID',supervisorId);
+
+        	}
+        };
+
+    });
