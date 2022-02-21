4ffa4fb03b8797ad61a183e2cce1f065d820f7cf
Michael McNamara <michael@oldmasterhands.com>
2022-02-21 14:36:07 -0800

    Edited customer script for log audit testing

--- src/FileCabinet/SuiteScripts/sdr_ue_customer.js
+++ src/FileCabinet/SuiteScripts/sdr_ue_customer.js
@@ -8,7 +8,17 @@
 
         return {
         	afterSubmit : function(context) {
-        		log.debug('Hello World');
+  //      		log.debug('Hello World');
+  var customer = context.newRecord;
+  var customerId = customer.getValue('id');
+  var customerEmail = customer.getValue('email');
+  var customerRep = customer.getText('salesrep');
+  var customerCoupCode = customer.getValue('custentity_sdr_coupon_code');
+
+  log.audit('Customer ID', customerId);
+  log.audit('Customer Email', customerEmail);
+  log.audit('Customer Sales Rep', customerRep);
+  log.audit('Customer Coupon Code', customerCoupCode);
 
         	}
         };
