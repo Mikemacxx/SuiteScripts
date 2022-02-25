e9b47dba843f4b3f0d3fcace44d8d87a077b3639
Michael McNamara <michael@oldmasterhands.com>
2022-02-24 18:46:24 -0800

    Removed getTex from UE  scrip. Conflict with geValue. Saved changes to CS script for validation.

--- src/FileCabinet/SuiteScripts/sdr_ue_employee.js
+++ src/FileCabinet/SuiteScripts/sdr_ue_employee.js
@@ -10,15 +10,13 @@
         	afterSubmit : function(context) {
  //       		log.debug('Hello World');
    var employee = context.newRecord;
-   var empCode        = employee.getValue('custentity_sdr_employee_code');
-   var supervisorName = employee.getText('supervisor');
-   var supervisorId   = employee.getValue('supervisor');
-
+   var supervisorId = employee.getValue('supervisor');
+   var empCode = employee.getValue('custentity_sdr_employee_code');
+  
+  
 
    log.debug('Employee Code',empCode);
-   log.debug('Supervisor Name',supervisorName);
    log.debug('Supervisor ID',supervisorId);
-
         	}
         };
 
