2040cc71bb64859309610da586f9e06e84606dce
Michael McNamara <michael@oldmasterhands.com>
2022-02-28 19:12:18 -0800

    Added feature to pull HR department phone cusom field and place it in the new phone record's phone field.

--- src/FileCabinet/SuiteScripts/sdr_ue_employee.js
+++ src/FileCabinet/SuiteScripts/sdr_ue_employee.js
@@ -24,13 +24,24 @@
    log.debug('Supervisor ID',supervisorId);
 
 if(context.type == context.UserEventType.CREATE) {
+  var department = record.load({
+    type: record.Type.DEPARTMENT,
+    id: 2,
+    isDynamic: true,
+});
+var departmentPhone = department.getValue('custrecord_dept_phone');
+
 var phoneCall = record.create({
-  type : record.Type.PHONE_CALL
+  type : record.Type.PHONE_CALL,
+  defaultValues : {
+    customform : -150
+  }
 });
 phoneCall.setValue('title', "Call HR for your benefis.");
 phoneCall.setValue('assigned', employee.id);
+phoneCall.setValue('phone',departmentPhone);  //COULD**USE
 phoneCall.save();
-
+log.debug('Call Created',phoneCall.getValue('title'));
 }
 }
         };
