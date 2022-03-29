63d05e7cf8ba12ad8cebbefae766f93c54b06293
Michael McNamara <michael@oldmasterhands.com>
2022-03-28 19:32:24 -0700

    Changed code to buildt a search for cases thhat is scheduled and deployed on Netsuite.

--- src/FileCabinet/SuiteScripts/sdr_ss_escalated_cases.js
+++ src/FileCabinet/SuiteScripts/sdr_ss_escalated_cases.js
@@ -1,6 +1,7 @@
 /**
  * @NApiVersion 2.0
  * @NScriptType ScheduledScript
+ * @NModuleScope SameAccount
  */
 define(['N/search'],  //Important! For testing in console change define to require
     /**
@@ -8,24 +9,61 @@ define(['N/search'],  //Important! For testing in console change define to requi
  */
     function(search) { 
 
-        /**
-         * Defines the Scheduled script trigger point.
-         * @param {Object} scriptContext
-         * @param {string} scriptContext.type - Script execution context. Use values from the scriptContext.InvocationType enum.
-         * @since 2015.2
-         */
-        function execute(scriptContext) {
-                var caseSearch = search.load({
-                    id : 'customsearchp_sdr_escalated_searches'
+        return {
+            execute : function(context) {
+                var caseSearch = search.create({
+                    type : search.Type.SUPPORT_CASE,
+                    filters : [
+                        search.createFilter({
+                            name : 'status',
+                            operator : search.Operator.ANY_OF,
+                            values : 3
+                        }),
+                        search.createFilter ({
+                            name : 'title',
+                            operator : search.Operator.HASKEYWORDS,
+                            values : 'Support'
+                        })
+                    ],
+                    columns : [
+                        search.createColumn({name : 'title'}),
+                        search.createColumn({name : 'startdate'}),
+                        search.createColumn({name : 'assigned'}),
+                        search.createColumn({name : 'status'}),
+                        search.createColumn({name : 'assigned'}),
+                        search.createColumn({name : 'status'}),
+                        search.createColumn({name : 'department', join : 'employee'}),
+                        search.createColumn({name : 'title', join : 'employee'})
+                    ]
                 });
-
-                var searchresults = caseSearch.run().getRange({
-                    start : 0,
-                    end : 9
+                var searchResults = casSearch.run().getRange({
+                    start: 0,
+                    end: 9
                 });
-                //Important! Tto test in browser or NS console must add breakpoint 'var x = 0;'
+                // THIS IS WHERE WE BEGIN A LOOP TO LOOP THROUGH RESULTS ROWS
+                  
+                for(i=0;i<searchResults.length;i++) {
+                    
+                  var subject = searchResults[i].getValue('title');
+                  var assignedTo = searchResults[i].getValue('assigned');
+                  var status = searchResults[i].getValue('status');
+                  var department = searchResults[i].getValue({
+                      name : 'department',
+                      join : 'employee'
+                  });
+                  var jobTitle = searchResults[i].getValue({
+                      name : 'title',
+                      join : 'employee'
+                  });
+                  log.debug('Case Info', 'Subject : ' + subject + '\n' +
+                  'Status : ' + status + '\n' +
+                  'Job Title : ' + jobTitle);
+      
+      
+                }
+            }
         }
 
-        return {execute : execute} //Important! This could be removed for testing in Debigger
+         
 
     });
