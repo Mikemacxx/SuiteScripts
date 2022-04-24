e06a88bdd57e08ed6eb699bcd19da5b42247b8cd
Michael McNamara <michael@oldmasterhands.com>
2022-04-24 16:14:07 -0700

    Updated sdr_sl_update_notes to use if satement to se if operation is GET or POST. get cretes the suitelet page and lods the fields nd default values of employee. POST will submit suitelet data entered back to the employee and redirect to employee page.

--- src/FileCabinet/SuiteScripts/sdr_sl_update_emp_notes.js
+++ src/FileCabinet/SuiteScripts/sdr_sl_update_emp_notes.js
@@ -2,9 +2,16 @@
  * @NApiVersion 2.x
  * @NScriptType Suitelet
  */
-define(['N/ui/serverWidget'],
+define(['N/ui/serverWidget','N/record','N/redirect'],
+/**     
+         * Defines the Suitelet script trigger point.
+         * @param {serverWidget} serverWidget
+         * @param {record} record
+         * @param {redirect} redirect
+         * @since 2015.2
+         */
     
-   function(serverWidget) {
+   function(serverWidget,record,redirect) {
         /**     
          * Defines the Suitelet script trigger point.
          * @param {Object} scriptContext
@@ -16,6 +23,12 @@ define(['N/ui/serverWidget'],
 
             var request = context.request;
             var response = context.response;
+
+            if(request.method == 'GET') {
+
+            var name = request.parameters.sdr_name;
+            var notes = request.parameters.sdr_notes;
+            var empId = request.parameters.sdr_empid;
 
             var form = serverWidget.createForm({
                 title : 'Update Employee Notes',
@@ -43,6 +56,11 @@ define(['N/ui/serverWidget'],
             form.addSubmitButton({
                 label : 'Continue'
             });
+
+            nameFld.defaultValue = name;
+            notesFld.defaultValue = notes;
+            empIdFld.defaultValue = empId;
+
             
             nameFld.updateDisplayType({
                 displayType : serverWidget.FieldDisplayType.INLINE
@@ -54,7 +72,24 @@ define(['N/ui/serverWidget'],
 
 
             response.writePage(form);
+        } else {
+            var empId = request.parameters.custpage_sdr_emp_id;
+            var notes = request.parameters.custpage_sdr_notes;
+            var employee = record.load({
+                type : record.Type.EMPLOYEE,
+                id : empId
+            });
+            employee.setValue('comments', notes);
+            employee.save();
+
+            redirect.toRecord({
+                id: empId,
+                type: record.Type.EMPLOYEE,
+                isEditMode: false
+            });
+
         }
+    }
 
         return {onRequest : onRequest};
 
